b1261e0c31c7fcefc03f5731f9f3df27fee5cdbd
Muayyat Billah <muayyat@outlook.com>
2019-11-08 08:04:57 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GraniteHouse_WebShop.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebEssentials.AspNetCore.Pwa;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
+
+namespace GraniteHouse_WebShop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.Configure<IdentityOptions>(options => {
+                
+            });
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.User.AllowedUserNameCharacters = null;
+                
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            services.AddHttpClient();
+            services.AddAuthorization();
+            services.AddProgressiveWebApp(new WebEssentials.AspNetCore.Pwa.PwaOptions
+            {
+                OfflineRoute = "/shared/offline"
+            });
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(@"/Data/Protection"))
+            .SetApplicationName("StyrinSeller");
+            services.ConfigureApplicationCookie(options => {
+                options.Cookie.Name = ".Styrin.";
+                options.ClaimsIssuer = "Styrin";
+                
+               
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else 
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+                
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
+                   name: "Products",
+                   template: "Items",
+                   defaults: new { controller = "Products", action = "Index" }
+                    );
+
+                routes.MapRoute(
+                    name: "productEdit",
+                    template: "Item/{link}",
+                    defaults: new { controller = "Products", action = "Item" });
+
+
+
+            });
+        }
+    }
+}
